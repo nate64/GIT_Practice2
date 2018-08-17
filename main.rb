58177ff257f18e7118d7b57380906041de2812fc
Nate <nathanestes@gmail.com>
2018-08-16 22:10:31 -0600

    1 add git help menu

---
+++ main.rb
@@ -0,0 +1,55 @@
+#just copied some code here for fun
+@contacts = []
+
+def menu
+  puts "====================="
+  puts "Contact List"
+  puts "1) Add contact"
+  puts "2) View all"
+  puts "3) Exit"
+  puts "====================="
+  user_selection
+end
+
+def user_selection
+  case gets.to_i
+  when 1
+    add_contact
+  when 2
+    view_all
+  when 3
+    puts "GOODBYE"
+    exit
+  else
+    puts "Invalid selection\n\n"
+    menu
+  end
+end
+
+def info(type)
+  puts "#{type}"
+  result = gets.strip
+  while result == ''
+    puts "Please provide a value for #{type}"
+    result = gets.strip
+  end
+
+  result
+end
+
+def add_contact
+  first_name = info('First Name')
+  last_name = info('Last Name')
+  email = info('Email')
+  @contacts << { first_name: first_name, last_name: last_name, email: email }
+end
+
+def view_all
+  @contacts.each do |contact|
+    puts "#{contact[:last_name]}, #{contact[:first_name]}\t #{contact[:email]}"
+  end
+end
+
+while true
+  menu
+end
