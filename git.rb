20ae32f34a33ce9a2e25fd1a3545c1fbe1ed5825
Nate <nathanestes@gmail.com>
2018-08-17 10:11:41 -0600

    messing_around_display git config

--- git.rb
+++ git.rb
@@ -5,7 +5,20 @@ module Git
 end
 
 module Git
+<<<<<<< HEAD
   def self.git_cmd(cmd)
     puts `git #{cmd} -h`
   end
 end
+=======
+  def self.puts_git(cmd)
+    puts `git #{cmd} -h`
+  end
+
+  def self.config
+    puts `git config -l`
+  end
+end
+
+def self.display_cmd(cmd)
+>>>>>>> messing_around_display git config
--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 #just copied some code here for fun
 
+Git.display_cmd(gets.strip)
+
 require 'colorize'
 require_relative 'git'
 
@@ -61,6 +63,29 @@ while true
   menu
 end
 
+<<<<<<< HEAD
 when 1
   puts 'Enter git command'.colorize(:green)
   Git.git_cmd(gets.strip)
+=======
+def self.menu
+  puts 'MAIN MENU'.colorize(:cyan)
+  puts '1: Enter git command'.colorize(:cyan)
+  puts '2: View git config'.colorize(:cyan)
+  puts '3: Exit'.colorize(:cyan)
+  choice = gets.to_i
+  case choice
+  when 1
+    puts 'Enter git command'.colorize(:green)
+    Git.puts_git(gets.strip)
+  when 2
+    Git.config
+  when 3
+    exit
+  else
+    puts 'Invalid choice'.colorize(:red)
+  end
+
+  menu
+end
+>>>>>>> messing_around_display git config
